ab242765809eb82393b929b225d6b7a7b0d286e6
Cesardarinel <cesardarinel@hotmail.com>
2013-03-05 11:14:47 -0400

    clase entidades
    O.o

--- SalesSoft_v2/Recursos/Conexion.cs
+++ SalesSoft_v2/Recursos/Conexion.cs
@@ -7,6 +7,7 @@ using MySql.Data.MySqlClient;
 
 namespace SalesSoft_v2.Recursos
 {
+   
     abstract class Conexion
     {
         /// <summary>
---
+++ SalesSoft_v2/Recursos/Entidades.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesSoft_v2.Recursos
+{
+    class Entidades
+    {
+        #region Propiedades
+        
+        #endregion
+        #region Constructor
+        
+        #endregion
+        #region Metodo
+        
+        #endregion
+    }
+}

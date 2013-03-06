2d12454fd287dbc9c13a86e8bf11119a779a2c51
Cesardarinel <cesardarinel@hotmail.com>
2013-03-05 20:00:06 -0400

    creando clases

--- SalesSoft_v2/Recursos/Entidades.cs
+++ SalesSoft_v2/Recursos/Entidades.cs
@@ -6,16 +6,99 @@ using System.Threading.Tasks;
 
 namespace SalesSoft_v2.Recursos
 {
-    class Entidades
+    class Entidad
     {
-        #region Propiedades
+        #region Campo
         
+        #endregion
+        #region Propiedades
+
         #endregion
         #region Constructor
-        
+
         #endregion
         #region Metodo
-        
+
+        #endregion
+    }
+
+    class Persona:Entidad
+    {
+        #region Campo
+
+        #endregion
+        #region Propiedades
+
+        #endregion
+        #region Constructor
+
+        #endregion
+        #region Metodo
+
+        #endregion
+    }
+
+    class Suplidor:Entidad
+    {
+        #region Campo
+
+        #endregion
+        #region Propiedades
+
+        #endregion
+        #region Constructor
+
+        #endregion
+        #region Metodo
+
+        #endregion
+    }
+
+    class Empleado:Persona
+    {
+        #region Campo
+
+        #endregion
+        #region Propiedades
+
+        #endregion
+        #region Constructor
+
+        #endregion
+        #region Metodo
+
+        #endregion
+    }
+
+    class Administrativo:Persona
+    {
+        #region Campo
+
+        #endregion
+        #region Propiedades
+
+        #endregion
+        #region Constructor
+
+        #endregion
+        #region Metodo
+
+        #endregion
+    }
+
+    class Cliente:Persona 
+    {
+        #region Campo
+
+        #endregion
+        #region Propiedades
+
+        #endregion
+        #region Constructor
+
+        #endregion
+        #region Metodo
+
         #endregion
     }
    
